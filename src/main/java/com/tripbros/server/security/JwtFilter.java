6a50179cc0e3e35e596f106be52ccd0eba863863
hwangjokim <hg7258@naver.com>
2024-02-02 00:15:10 +0900

    Fix : JWT filter 중복 호출 수정

--- src/main/java/com/tripbros/server/security/JwtFilter.java
+++ src/main/java/com/tripbros/server/security/JwtFilter.java
@@ -18,7 +18,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtFilter extends OncePerRequestFilter {
