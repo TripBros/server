1e7f16e0063718dcdfdc3b55450b06e4407f3c85
rladmstn <oz33177351@gmail.com>
2024-02-28 19:14:34 +0900

    add : 추천 맛집 정보에 별점 정보 추가

--- src/main/java/com/tripbros/server/recommend/dto/UpdateBookmarkedPlaceRequestDTO.java
+++ src/main/java/com/tripbros/server/recommend/dto/UpdateBookmarkedPlaceRequestDTO.java
@@ -3,8 +3,9 @@ package com.tripbros.server.recommend.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateBookmarkedPlaceRequestDTO(@NotNull(message = "지역 id는 필수 값입니다.") Long locateId,
-											  @NotNull(message = "장소 id는 필수 값입니다.") Long placeId,
+											  @NotNull(message = "장소 id는 필수 값입니다.") String placeId,
 											  String placeName,
 											  String placeUrl,
+											  Float placeRating,
 											  String placeImage) {
 }
